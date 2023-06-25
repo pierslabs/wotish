e4dd3dcb257d25a24183b034b6a7d8ace66e5de6
pierslabs <pedrolosasp@gmail.com>
2023-06-25 10:56:34 +0200

    add supabase

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,25 @@
+import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { supabase } from "../db/supabase";
+
+const useAuth = () => {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return { session, supabase };
+};
+
+export default useAuth;
