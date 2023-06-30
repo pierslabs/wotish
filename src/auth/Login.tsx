5f85d28f5cb4db8d9d2c2bb038cb5ba6dba2d4bd
pierslabs <pedrolosasp@gmail.com>
2023-07-01 01:11:00 +0200

    add login styles

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,10 +1,30 @@
-import { Auth } from "@supabase/auth-ui-react";
-import useAuth from "../hooks/useAuth";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Auth } from '@supabase/auth-ui-react';
+import useAuth from '../hooks/useAuth';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
 
 const Login = () => {
   const { supabase } = useAuth();
-  return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  return (
+    <div
+      className='flex flex-col align-middle justify-center border  bg-black h-100 '
+      style={{ height: '100vh' }}
+    >
+      <img
+        src={' /assets/wotish.png'}
+        alt='wotish logo'
+        className='mx-auto sm:w-1/3 w-3/4'
+      />
+      <div className='sm:mx-auto sm:w-1/3  mx-auto w-3/4 mt-12'>
+        <Auth
+          providers={['google']}
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+          showLinks={false}
+          onlyThirdPartyProviders
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
