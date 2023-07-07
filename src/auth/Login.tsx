00bdbf9a5898ead5dcd04e462fcb82c969d7bae4
pierslabs <pedrolosasp@gmail.com>
2023-07-07 16:53:34 +0200

    login styles

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -7,23 +7,23 @@ const Login = () => {
   console.log('WeLcome to Login');
   const { supabase } = useAuth();
   return (
-    <div
-      className='flex flex-col align-middle justify-center  h-100 p-2'
-      style={{ height: '100vh' }}
-    >
-      <div className='absolute top-2'>
+    <div>
+      <div className=' flex justify-end items-center p-2'>
         <AiOutlineFieldTime size={50} color='blue' />
-        <h1 className='text-xl font-bold  absolute ml-2 left-12 top-3'>
-          Wotish
-        </h1>
+        <h1 className='text-xl font-bold  ml-1 '>Wotish</h1>
       </div>
-      <div className='sm:mx-auto sm:w-1/3  mx-auto w-3/4 mt-12'>
-        <Auth
-          providers={['google']}
-          supabaseClient={supabase}
-          appearance={{ theme: ThemeSupa }}
-          showLinks={true}
-        />
+      <div
+        className='flex flex-col align-middle justify-center  h-100 p-2 bg-gradient-to-b from-blue-800 '
+        style={{ height: '100vh' }}
+      >
+        <div className='sm:mx-auto md:w-1/3   w-full  mx-auto mt-12 border bg-zinc-50  p-3 '>
+          <Auth
+            providers={['google']}
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            showLinks={true}
+          />
+        </div>
       </div>
     </div>
   );
