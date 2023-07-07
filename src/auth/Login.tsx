ca463b6f7682d8e56319020c98cf4847050ed76c
pierslabs <pedrolosasp@gmail.com>
2023-07-07 17:53:37 +0200

    fixed in navbar

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -8,15 +8,15 @@ const Login = () => {
   const { supabase } = useAuth();
   return (
     <div>
-      <div className=' flex justify-end items-center p-2'>
+      <div className=' flex justify-end items-center p-2 fixed bg-white w-full'>
         <AiOutlineFieldTime size={50} color='blue' />
         <h1 className='text-xl font-bold  ml-1 '>Wotish</h1>
       </div>
       <div
-        className='flex flex-col align-middle justify-center  h-100 p-2 bg-gradient-to-b from-blue-800 '
+        className='flex flex-col  justify-center h-100 p-2 bg-gradient-to-b from-blue-800 '
         style={{ height: '100vh' }}
       >
-        <div className='sm:mx-auto md:w-1/3   w-full  mx-auto mt-12 border bg-zinc-50  p-3 '>
+        <div className='sm:mx-auto md:w-1/3 w-full mx-auto  border bg-zinc-50  p-3 '>
           <Auth
             providers={['google']}
             supabaseClient={supabase}
