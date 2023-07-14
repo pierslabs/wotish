ab1e91e9a60306cf24ee91ef6a3efbe6af650822
pier slabs <pedrolosasp@gmail.com>
2023-07-15 00:39:15 +0200

    simple calendar prueba

--- src/clocker/pages/WorkRecord.tsx
+++ src/clocker/pages/WorkRecord.tsx
@@ -1,9 +1,15 @@
 import { Layout } from '../components/Layout';
-
+import 'react-modern-calendar-datepicker/lib/DatePicker.css';
+import { Calendar } from 'react-modern-calendar-datepicker';
 const WorkRecord = () => {
   return (
     <Layout>
-      <div>WorkRecord</div>;
+      <div
+        className='bg-green-100 p-1 waves'
+        style={{ height: 'calc(100vh - 60px)' }}
+      >
+        <Calendar calendarClassName='responsive-calendar' />
+      </div>
     </Layout>
   );
 };
