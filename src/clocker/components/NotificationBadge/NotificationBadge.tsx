a1c12293d3707afff3afc4386cfa467749932ae6
pierslabs <pedrolosasp@gmail.com>
2023-07-15 17:25:55 +0200

    notifications w-full container

--- src/clocker/components/NotificationBadge/NotificationBadge.tsx
+++ src/clocker/components/NotificationBadge/NotificationBadge.tsx
@@ -36,7 +36,7 @@ const NotificationBadge = () => {
     >
       <MdNotificationsNone
         size={notificationData.open ? 35 : 30}
-        color={'#ffee01d6'}
+        color={'#fff701'}
         style={{ transition: 'all 0.3s ease' }}
         className={`cursor-pointer ${notificationData.open && 'animate-pulse'}`}
       />
@@ -48,11 +48,11 @@ const NotificationBadge = () => {
       )}
       {notificationData.open && (
         <div className='relative'>
-          <div className='absolute w-40 h-20 right-12 -bottom-12 p-2  rounded-lg bg-gray-50 border'>
+          <div className='absolute sm:w-72 w-60 right-12 -top-8 p-2  rounded-lg bg-gray-50 border'>
             <div className='relative'>
               <div className='w-6 h-6 bg-gray-50 transform rotate-45 absolute -top-3 right-[-18px] m-2'></div>
 
-              <h2 className='mb-4 ' onClick={redirectToWorkRecord}>
+              <h2 className='h-auto p-3' onClick={redirectToWorkRecord}>
                 Tienes nuevos <span className='underline'>turnos</span>{' '}
                 asignados
               </h2>
